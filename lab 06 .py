11d5827a9ad33f8fc72596d75cc04a46c14fde1d
Diego Valdivia <dvaldivia@ufl.edu>
2023-03-22 18:07:21 -0400

    fixed a small mistake in decode function.

--- lab 06 .py
+++ lab 06 .py
@@ -22,7 +22,11 @@ def encode(password_input):
 def decode(encoded):
     password_decoded = ''
     for char in encoded:
-        password_decoded += (str(int(char) - 3))  # subtracts 3 to each number in the password
+        if (int(char) - 3) < 0:
+            password_decoded += (str((int(char) - 3) + 10))
+
+        else:
+            password_decoded += (str(int(char) - 3))  # subtracts 3 to each number in the password
     return password_decoded
 
 
